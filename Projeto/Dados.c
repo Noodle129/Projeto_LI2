5cfbaefe3e8c93491cd7a556d41da34fa7caca46
Noodle129 <61905786+Noodle129@users.noreply.github.com>
2020-03-12 13:01:01 +0000

    1.º

---
+++ Projeto/Dados.c
@@ -0,0 +1,22 @@
+#ifndef LI2G09_STRUCT_FILE_H
+#define LI2G09_STRUCT_FILE_H
+
+/*
+ *
+ESTADO *inicializar_estado();
+int obter_jogador_atual(ESTADO *estado);
+int obter_numero_de_jogadas(ESTADO *estado);
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
+inicializar_estado Esta função deverá criar um estado vazio (com o tabuleiro
+inicializado)
+obter_jogador_atual Esta função permite obter o número do jogador atual
+obter_estado_casa Esta função permite obter o estado atual da casa
+obter_numero_de_jogadas Esta função permite obter quantas jogadas
+        foram efetuadas (cada jogada tem o movimento de dois jogadores)
+
+
+
+*/
+
+
+#endif //LI2G09_STRUCT_FILE_H
---
+++ Projeto/Interface.h
@@ -0,0 +1,8 @@
+//
+// Created by Lídia Anaís Coelho de Sousa on 12/03/2020.
+//
+
+#ifndef PROJETO_INTERFACE_H
+#define PROJETO_INTERFACE_H
+
+#endif //PROJETO_INTERFACE_H
