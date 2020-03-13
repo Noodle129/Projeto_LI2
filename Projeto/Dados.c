6e0e064de7f9c1b448d78742f433e557360a7317
Noodle129 <61905786+Noodle129@users.noreply.github.com>
2020-03-13 15:05:29 +0000

    Update Dados.c
    Sem erros pela função inicializar_estado

--- Projeto/Dados.c
+++ Projeto/Dados.c
@@ -1,5 +1,6 @@
 #ifndef LI2G09_STRUCT_FILE_H
 #define LI2G09_STRUCT_FILE_H
+
 #include <stdio.h>
 #include "Dados.h"
 #include <stdlib.h>
@@ -8,7 +9,28 @@
 #include "logica.h"
 #include "Interface.h"
 
-int obter_jogador_atual(ESTADO *estado)(){
+ESTADO *inicializar_estado()
+{
+    ESTADO *estadoinicial;
+
+    estadoinicial = &((ESTADO){.tab = {
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, BRANCA, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO},
+            {VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO, VAZIO}},
+            .ultima_jogada = {4, 3},
+            .jogadas = {},
+            .num_jogadas = 0,
+            .jogador_atual = 1});
+
+    return estadoinicial;
+}
+
+int obter_jogador_atual(ESTADO *estado){
     int ja = estado->jogador_atual;
     return ja;
 }
@@ -24,27 +46,9 @@ COORDENADA obter_ultima_jogada(ESTADO *estado){
 }
 
 CASA obter_estado_casa(ESTADO *e, COORDENADA c){
-    CASA *tabu = e->tab;
-    CASA casa = tabu[c.coluna][c.linha];
-    return casa;
+    CASA cestado = e->tab[c.coluna][c.linha];
+    return cestado;
 }
-
-ESTADO *inicializar_estado() {
-    ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
-    e->jogador_atual = 1;
-    e->num_jogadas = 0;
-    e->jogadas;
-    e->tab = {{VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,BRANCA,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO},
-              {VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO,VAZIO}};
-    return e;
-}
-
 
 
 #endif //LI2G09_STRUCT_FILE_H
